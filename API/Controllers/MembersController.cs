4d5df7ba072c5df5370eb655e8a9a1463970a584
Aharon Moskovitsh <f05485@gmail.com>
2025-10-19 12:14:42 +0300

    Implement BaseApiController and refactor Members and WeatherForecast controllers to inherit from it

--- API/Controllers/MembersController.cs
+++ API/Controllers/MembersController.cs
@@ -1,13 +1,10 @@
 using API.Data;
 using API.Entities;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class MembersController(AppDbContext context) : ControllerBase
+        public class MembersController(AppDbContext context) : BaseApiController
     {
         [HttpGet] 
         public ActionResult<IReadOnlyList<AppUser>> GetMembers()
