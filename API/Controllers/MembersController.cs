0a8bc7aacdcc0975aa1ca42770055a0ede9384e2
Aharon Moskovitsh <f05485@gmail.com>
2025-10-23 15:41:55 +0300

    Adding authentication middleware

--- API/Controllers/MembersController.cs
+++ API/Controllers/MembersController.cs
@@ -1,17 +1,19 @@
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
         public class MembersController(AppDbContext context) : BaseApiController
     {
-        [HttpGet] 
+        [HttpGet]
         public ActionResult<IReadOnlyList<AppUser>> GetMembers()
         {
             var users = context.Users.ToList();
             return Ok(users);
         }
+        [Authorize]
         [HttpGet("{id}")]
         public ActionResult<AppUser> GetMembers(string id)
         {
